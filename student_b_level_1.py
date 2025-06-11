08bba5d7addc470c52e5544eaaae6e82d6ff14b2
qil037Hua <s4173926@student.rmit.edu.au>
2025-06-11 16:12:34 +1000

    a

--- student_b_level_1.py
+++ student_b_level_1.py
@@ -3,10 +3,20 @@ def get_page_html(form_data):
     page_html="""<!DOCTYPE html>
     <html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>About us</title>
+        <link rel="stylesheet" href="/style.css">
     </head>
     <body>
         <h1>About this website</h1>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/page2a">Page 2A</a></li>
+            <li><a href="/page3a">Page 3A</a></li>
+            <li><a href="/page1b">Page 1B</a></li>
+            <li><a href="/page2b">Page 2B</a></li>
+            <li><a href="/page3b">Page 3B</a></li>
+        </ul>
         <p>This is the first dynamically generated page!</p>
         <p><a href="/">Go to Page 1A</a></p>
         <p><a href="/page2a">Go to Page 2A</a></p>
